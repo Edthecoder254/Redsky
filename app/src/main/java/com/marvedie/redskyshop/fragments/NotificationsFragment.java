3b6e81fcaf73f386be5d3883bb864722e3e2dcb1
Marvedie <marvedie254@gmail.com>
2018-09-27 12:51:53 +0300

    Register/Login update

--- app/src/main/java/com/marvedie/redskyshop/fragments/NotificationsFragment.java
+++ app/src/main/java/com/marvedie/redskyshop/fragments/NotificationsFragment.java
@@ -1,19 +1,54 @@
 package com.marvedie.redskyshop.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.marvedie.redskyshop.LoginActivity;
 import com.marvedie.redskyshop.R;
+import com.marvedie.redskyshop.RegisterActivity;
 
 
 public class NotificationsFragment extends Fragment {
-    @Nullable
+
+    //Define Views
+    Button btnLogin;
+    Button btnRegister;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notifications, null);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+
     }
-}
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+            btnLogin = view.findViewById(R.id.btnLogin);
+            btnRegister = view.findViewById(R.id.btnLogin);
+
+
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity( new Intent(getActivity(), LoginActivity.class));
+                }
+            });
+
+            btnRegister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    startActivity(new Intent(getActivity(), RegisterActivity.class));
+                }
+            });
+            return view;
+        }
+
+    }
