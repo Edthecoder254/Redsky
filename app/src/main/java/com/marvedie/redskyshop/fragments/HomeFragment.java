d11bec60887a793c83f8de4a636eff84f3e0e0c1
Marvedie <marvedie254@gmail.com>
2018-09-27 11:09:26 +0300

    2

---
+++ app/src/main/java/com/marvedie/redskyshop/fragments/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.marvedie.redskyshop.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.marvedie.redskyshop.R;
+
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //just change the fragment_dashboard
+        //with the fragment you want to inflate
+        //like if the class is HomeFragment it should have R.layout.home_fragment
+        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+        return inflater.inflate(R.layout.fragment_home, null);
+    }
+}
