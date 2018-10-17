541e7596a144608a546b6e7082715a9b69d1ccf8
Marvedie <marvedie254@gmail.com>
2018-10-17 10:17:03 +0300

    Shopping cart working

---
+++ app/src/main/java/com/marvedie/redskyshop/shoppingcart/jsonEntityObjects/ServerObject.java
@@ -0,0 +1,16 @@
+package com.marvedie.redskyshop.shoppingcart.jsonEntityObjects;
+
+public class ServerObject {
+
+    private String success;
+
+    public ServerObject(String success) {
+        this.success = success;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+}
+
+
